7527333bddbe939fde7a72f6d6cd1c8f3680f444
Yeison Lopez <yeison.lopez1028@gmail.com>
2026-04-07 15:01:55 -0400

    feat(python-cicd)formatting and linting checks

--- src/simple_http_checker/checker.py
+++ src/simple_http_checker/checker.py
@@ -4,6 +4,7 @@ from typing import Collection
 
 logger = logging.getLogger(__name__)
 
+
 def check_urls(urls: Collection[str], timeout: int = 5) -> dict[str, str]:
     """
     Check the status of a list of URLs.
@@ -17,8 +18,8 @@ def check_urls(urls: Collection[str], timeout: int = 5) -> dict[str, str]:
     """
 
     logger.info(f"Checking {len(urls)} URLs with a timeout of {timeout} seconds.")
-    
-    results = {}
+
+    results: dict[str, str] = {}
     for url in urls:
         status = "UNKNOWN"
 
@@ -31,16 +32,16 @@ def check_urls(urls: Collection[str], timeout: int = 5) -> dict[str, str]:
                 status = f"FAIL (Status Code: {response.status_code}) {response.reason}"
         except requests.exceptions.Timeout as e:
             logger.error(f"Error checking URL {url}: {e}")
-            status = f"TIMEOUT"
+            status = f"TIMEOUT after {timeout} seconds"
             logger.warning(f"URL {url} timed out after {timeout} seconds.")
         except requests.exceptions.ConnectionError as e:
             logger.error(f"Error checking URL {url}: {e}")
-            status = f"CONNECTION ERROR"
+            status = f"CONNECTION ERROR: {type(e).__name__}"
             logger.warning(f"URL {url} could not be reached.")
         except requests.exceptions.RequestException as e:
             status = f"REQUEST ERROR: {type(e).__name__}"
             logger.error(f"Error checking URL {url}: {e}", exc_info=True)
-        
+
         results[url] = status
         logger.debug(f"Checked: {url:<40} -> {status}")
 
